642da1e3a7231249d4bc6fb53a9df2650ec3ffe4
AmazingBrice <brice.folleas@gmail.com>
2020-10-16 15:59:22 +0200

    Add exo3

---
+++ TP-5-Signaux/exo3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+int fin = 1;
+
+void redirect (int signal) {
+    if (signal == 2){
+        printf("Reception signal SIGINT\n");
+        printf("Set bool to true");
+        fin = 0;
+    }
+}
+
+int main()
+{
+    //Si Le signal SIGINT est envoyé, il est redirigé vers redirect
+    struct sigaction prepaSignal; // Déclaration d'une structure sigaction pour la mise en place des gestionnaires
+  
+    prepaSignal.sa_handler=&redirect;
+     // Remplissage de la structure avec l'adresse du gestionnaire (redirect)
+    prepaSignal.sa_flags=0; // Mise a zéro du champ sa_flags theoriquement ignoré
+    sigemptyset(&prepaSignal.sa_mask); // On initialise le masque à vide
+    sigaddset(&prepaSignal.sa_mask,SIGINT); // On ajoute au masque le signal SIGINT
+
+    while(fin){
+        printf("toto\t");
+    } // En attente d'une interruption
+    return 0;
+}
